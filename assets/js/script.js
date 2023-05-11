173f36ebacd38d302d62431ee36ba3cf3650bf7b
agalindo2014 <aprilgalindo2014@gmail.com>
2023-05-11 17:28:38 -0500

    updated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 // Define the quiz questions as an array of objects
 const quizQuestions = [
     {
-      question: "What is the output of 1 + 1?",
-      options: ["1", "2", "3", "4"],
-      answerIndex: 1
+      question: "Which of these is a programming language?",
+      options: ["bite", "itch", "gnaw", "scratch"],
+      answerIndex: 4
     },
     {
-      question: "What is the result of 'foo' + 'bar'?",
-      options: ["foobar", "foo bar", "foo+bar", "NaN"],
-      answerIndex: 0
+      question: "People who write computer code are?",
+      options: ["profssors", "instructors", "programmers", "manufacturers"],
+      answerIndex: 3
     },
     {
-      question: "What is the value of the global 'NaN' property?",
-      options: ["undefined", "null", "0", "Not a Number"],
+      question: "Which of these is not a programming language?",
+      options: ["Python", "Java", "Banana", "Ruby"],
       answerIndex: 3
     },
     // Add more questions here
